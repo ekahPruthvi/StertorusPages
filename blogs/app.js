0511d8dccc667bd202f1c5a2f5d5b9597dffbdba
ekahPruthvi <p1sribhat@gmail.com>
2025-10-23 20:34:01 +0530

    folders

---
+++ blogs/app.js
@@ -0,0 +1,22 @@
+let current = null;
+function expandFolder(i) {
+  document.querySelectorAll('.folder-tab').forEach(e => e.classList.remove('active'));
+  document.querySelectorAll('.folder-content').forEach(e => e.classList.remove('active'));
+  
+  document.querySelectorAll('.folder-tab')[i].classList.add('active');
+  document.getElementById('folder-content-' + i).classList.add('active');
+}
+
+expandFolder(0);
+
+function AddAnimatedCircle() {
+  const circle = document.getElementById('circleId');
+  document.addEventListener('mousemove', (e) => {
+    requestAnimationFrame(() => {
+      circle.style.left = `${e.clientX}px`;
+      circle.style.top = `${e.clientY}px`;
+    });
+  });
+}
+
+AddAnimatedCircle();
