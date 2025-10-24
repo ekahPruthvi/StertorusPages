0190e8bdfdc111f88581940c1c8e510318c32647
ekahPruthvi <p1sribhat@gmail.com>
2025-10-24 07:50:05 +0530

    making book

--- blogs/app.js
+++ blogs/app.js
@@ -1,8 +1,9 @@
 let current = null;
 function expandFolder(i) {
   document.querySelectorAll('.folder-tab').forEach(e => e.classList.remove('active'));
+  document.querySelectorAll('.folder-tab-body').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-content').forEach(e => e.classList.remove('active'));
-  
+  document.querySelectorAll('.folder-tab-body')[i].classList.add('active');
   document.querySelectorAll('.folder-tab')[i].classList.add('active');
   document.getElementById('folder-content-' + i).classList.add('active');
 }
