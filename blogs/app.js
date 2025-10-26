9cc522e99ff406ba1dcbd5139104c796f7613dbd
ekahPruthvi <p1sribhat@gmail.com>
2025-10-26 14:39:41 +0530

    new index bug fix

--- blogs/app.js
+++ blogs/app.js
@@ -5,11 +5,15 @@ function expandFolder(i) {
   previousIndex = currentIndex;
   currentIndex = i;
   console.log(previousIndex)
+
+  document.getElementById('legend').style.visibility = 'hidden';
+  document.getElementById('index').style.visibility = 'hidden';
+
   document.querySelectorAll('.folder-tab').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-tab-body').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-content').forEach(e => e.classList.remove('active'));
-  document.querySelectorAll('.folder-tab-body')[i].classList.add('active');
-  document.querySelectorAll('.folder-tab')[i].classList.add('active');
+  document.querySelectorAll('.folder-tab-body')[i-1].classList.add('active');
+  document.querySelectorAll('.folder-tab')[i-1].classList.add('active');
   document.getElementById('folder-content-' + i).classList.add('active');
 }
 
