52ba9bedfdbcba789dd9aae278fb21ca841702c9
ekahPruthvi <p1sribhat@gmail.com>
2025-10-26 08:50:29 +0530

    back jumping with alt+<-

--- blogs/app.js
+++ blogs/app.js
@@ -1,5 +1,10 @@
-let current = null;
+let previousIndex = null;
+let currentIndex = null;
+
 function expandFolder(i) {
+  previousIndex = currentIndex;
+  currentIndex = i;
+  console.log(previousIndex)
   document.querySelectorAll('.folder-tab').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-tab-body').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-content').forEach(e => e.classList.remove('active'));
@@ -9,11 +14,32 @@ function expandFolder(i) {
 }
 
 expandFolder(0);
+currentIndex = 0
+// pageMove(1.2)
 
-function pagemove(i) {
+function pageMove(i) {
+  previousIndex = currentIndex;
+  currentIndex = i;
+  console.log(previousIndex)
   document.querySelectorAll('.folder-tab').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-tab-body').forEach(e => e.classList.remove('active'));
   document.querySelectorAll('.folder-content').forEach(e => e.classList.remove('active'));
   document.getElementById('inner-link-page-' + i).classList.add('active');
 }
 
+function goBack() {
+  if (previousIndex === null) return;
+
+  if (previousIndex % 1 !== 0) {
+    pageMove(previousIndex);
+  } else {
+    expandFolder(parseInt(previousIndex));
+  }
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.altKey && event.code === 'ArrowLeft') {
+    event.preventDefault();
+    goBack();
+  }
+});
